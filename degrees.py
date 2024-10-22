22771cbbe14e1aad46117a24d09af49d7d2df13b
Nick Raptis <nraptis@gmail.com>
2024-10-22 16:19:15 -0400

    done some

--- degrees.py
+++ degrees.py
@@ -12,7 +12,6 @@ people = {}
 # Maps movie_ids to a dictionary of: title, year, stars (a set of person_ids)
 movies = {}
 
-
 def load_data(directory):
     """
     Load data from CSV files into memory.
@@ -53,19 +52,28 @@ def load_data(directory):
 
 
 def main():
-    if len(sys.argv) > 2:
-        sys.exit("Usage: python degrees.py [directory]")
+
     directory = sys.argv[1] if len(sys.argv) == 2 else "large"
+
+    print("argz ", sys.argv)
 
     # Load data from files into memory
     print("Loading data...")
     load_data(directory)
     print("Data loaded.")
 
-    source = person_id_for_name(input("Name: "))
+    #source = person_id_for_name(input("Name: "))
+    source = person_id_for_name(sys.argv[2])
+
     if source is None:
         sys.exit("Person not found.")
-    target = person_id_for_name(input("Name: "))
+    #target = person_id_for_name(input("Name: "))
+    target = person_id_for_name(sys.argv[3])
+
+    print("Using this data source: ", directory)
+    print("Using this Person A: ", sys.argv[2])
+    print("Using this Person B: ", sys.argv[3])
+    
     if target is None:
         sys.exit("Person not found.")
 
@@ -83,6 +91,25 @@ def main():
             movie = movies[path[i + 1][0]]["title"]
             print(f"{i + 1}: {person1} and {person2} starred in {movie}")
 
+class PathNode:
+    def __init__(self, person_id: str, movie_id: str, parent: 'PathNode' = None):
+        self.person_id = person_id
+        self.movie_id = movie_id
+        self.parent = parent
+
+    # Equality check based on person_id and movie_id
+    def __eq__(self, other):
+        if isinstance(other, PathNode):
+            return self.person_id == other.person_id and self.movie_id == other.movie_id
+        return False
+
+    # Hashing based on person_id and movie_id
+    def __hash__(self):
+        return hash((self.person_id, self.movie_id))
+
+    # Optional: for better debugging and printing
+    def __repr__(self):
+        return f"PathNode(person_id={self.person_id}, movie_id={self.movie_id}, parent={self.parent})"
 
 def shortest_path(source, target):
     """
@@ -92,8 +119,64 @@ def shortest_path(source, target):
     If no possible path, returns None.
     """
 
-    # TODO
-    raise NotImplementedError
+    print("Finding shotest path from ", source, " to ", target)
+
+    frontier = QueueFrontier()
+
+    start_node = PathNode(source, movie_id=None, parent=None)
+
+    frontier.add(start_node)
+
+    visited_person_id_set = set()
+    visited_person_id_set.add(source)
+
+    print("AAA, AAA, AAA")
+    while frontier.empty() == False:
+        popped_node = frontier.remove()
+        print("BBB, BBB, BBB, Popped node is ", popped_node)
+
+        neighbors = neighbors_for_person(popped_node.person_id)
+        print("neighbors = ", neighbors)
+
+        for neighbor in neighbors:
+            neighbor_person_id = neighbor[1]
+            neighbor_movie_id = neighbor[0]
+
+            print("neighbor_person_id = ", neighbor_person_id)
+            print("neighbor_movie_id = ", neighbor_movie_id)
+            
+    
+
+
+    print(people[source])
+    print(people[target])
+    
+
+
+    return [("93779", "914612"), ("112384", "1697")]
+
+'''
+class StackFrontier():
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
+'''
 
 
 def person_id_for_name(name):
